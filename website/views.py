a7ce1385d54172bced64e85d97847d0dffacbb44
Tae Yeong Kim <taeyeong2000@gmail.com>
2022-02-09 23:45:40 +1100

    created basic layout

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
